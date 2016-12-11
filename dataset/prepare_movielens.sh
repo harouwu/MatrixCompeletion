97e39cf10e24417936a7e8446a5cc3a06e5ee930
harouwu <Yiming@WuYimings-MBP.wv.cc.cmu.edu>
2016-12-11 16:45:24 -0500

    movie_lens_dataset

---
+++ dataset/prepare_movielens.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+echo "Making Directory"
+mkdir -p movie_lens
+
+if [ ! -f /movie_lens/ml-20m.zip ]; then
+    echo "Downloading Movielens"
+    wget http://files.grouplens.org/datasets/movielens/ml-20m.zip -P movie_lens
+fi
+
+cd movie_lens
+
+unzip ml-20m.zip
